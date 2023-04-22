f0902c71ebfec3f00c6b855321d7fb5f32b7f438
dangercrow <dangercrow@gmail.com>
2023-04-22 11:17:35 +0100

    Get it running locally

--- Gw2LauncherNewStyle/Properties/Resources.Designer.cs
+++ Gw2LauncherNewStyle/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Gw2Launcher.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gw2Launcher.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gw2LauncherNewStyle.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
